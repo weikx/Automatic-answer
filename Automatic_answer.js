e29766bbc65bbd0ac56147b7dafb3a4e4a3ef353
weikx <weikexin666@gmail.com>
2019-08-18 11:23:45 +0800

    Update Automatic_answer.js
    修改注释
    使用es6重写

--- Automatic_answer.js
+++ Automatic_answer.js
@@ -1,8 +1,13 @@
-// 运行环境：安卓软件Auto.js(https://github.com/hyb1996/Auto.js), 下载地址: https://github.com/hyb1996/Auto.js/releases
-// 需求root权限或者安卓7.0以上才能运行本脚本
+/**
+ * 此代码为2018年初用于网络活动自动答题脚本
+ * 现已无用，仅供参考
+ * 
+ * 运行环境：安卓软件 Auto.js(https://github.com/hyb1996/Auto.js)
+ * 需求root权限或者安卓7.0以上才能运行本脚本
+ */
 
-var url = "http://answer.sm.cn/answer/curr?activity=million&activity=million";
-var thisRound = 0;
+const url = "http://answer.sm.cn/answer/curr?activity=million&activity=million";
+let thisRound = 0;
 
 setScreenMetrics(device.width, device.height);
 
@@ -11,52 +16,45 @@ log("准备开始答题喽~");
 log("祝你好运~");
 log("Code By Weikexin");
 
-setInterval(function () {
+setInterval(() => {
   // 轮询请求接口
-    var res = http.get(url);
-    if (res.statusCode == 200) {
-        var data = JSON.parse(res.body.string());
-        if (data.data.correct) {
-            if (Number(data.data.round) > thisRound) {
-                thisRound = Number(data.data.round) ;
-                var correct = Number(data.data.correct)+1;
-                clickNow(thisRound, correct);
-            }
-        } else {
-            // toast("答题还未开始")
-        }
-    } else {
-        toast("请求失败:" + res.statusMessage);
+  const res = http.get(url);
+  if (res.statusCode == 200) {
+    const data = JSON.parse(res.body.string()).data;
+    if (data.correct && Number(data.round) > thisRound) {
+      thisRound = Number(data.round);
+      let correct = Number(data.correct) + 1;
+      clickNow(thisRound, correct);
     }
-},300)
+  } else {
+    toast(`请求失败: ${res.statusMessage}`);
+  }
+}, 300)
 
-function clickNow(round, correct) {
+function clickNow (round, correct) {
   // 自动点击方法
-  log("第"+ round +"题"+ "选"+correct);
-  if (correct == 1) {
-    click(550, 790);
-    click(550, 790);
-    log("已自动为您选择");
+  log(`第${round}题选${correct}`);
+  switch (correct) {
+    case 1:
+      click(550, 790);
+      break
+    case 2:
+      click(550, 980);
+      break
+    case 3:
+      click(550, 1160);
+      break
   }
-  if (correct == 2) {
-    click(550, 980);
-    click(550, 980);
-    log("已自动为您选择");
-  }
-  if (correct == 3) {
-    click(550, 1160);
-    click(550, 1160);
-    log("已自动为您选择");
-  }
+  log("已自动为您选择");
 }
 
 //启用按键监听
 events.observeKey();
-events.onKeyDown("volume_down", function(event){
-    engines.stopAllAndToast();
-    log("您停止了程序");
+events.onKeyDown("volume_down", event => {
+  engines.stopAllAndToast();
+  log("您停止了程序");
 });
 
-setInterval(function(){
+setInterval(() => {
   toast("答题程序运行中~");
-},20000)
+}, 20000)
